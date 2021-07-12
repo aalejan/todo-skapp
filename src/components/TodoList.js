0b0b4f399d4ec8a7d3c4f1a658b6bc8796fccce3
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-07-12 14:45:08 -0400

    created a function that will handle input change on form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,22 @@
+import { useState } from "react"
 
 const TodoForm = () => {
-    
+    const [todo, setTodo] = useState({
+        id: '',
+        task: ''
+    })
+
+    function handleInputChange(e){
+        setTodo({...todo, task: e.target.value })
+    }
+
+    return(
+        <form>
+            <input
+            name='task'
+            value={todo.task}
+             onChange={handleInputChange} type="text" />
+            <button type='submit' />
+        </form>
+    )
 }

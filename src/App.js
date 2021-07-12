43aeebf32fb6a55b9aef23c6db7a0594a9044458
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-07-12 00:27:38 -0400

    imported skynet client

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// Import the SkynetClient and a helper
+import { SkynetClient } from 'skynet-js';
+
+// We'll define a portal to allow for developing on localhost.
+// When hosted on a skynet portal, SkynetClient doesn't need any arguments.
+const portal = 'https://siasky.net/';
+
+// Initiate the SkynetClient
+const client = new SkynetClient(portal);
+
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
